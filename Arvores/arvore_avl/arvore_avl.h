851ef09005536b10678f91cde67ad534b22de86b
albanomatheus <albanomatheus16@gmail.com>
2018-06-28 23:02:10 -0300

    Funções básicas da Ávore AVL funcionando

--- Arvores/arvore_avl/arvore_avl.h
+++ Arvores/arvore_avl/arvore_avl.h
@@ -2,10 +2,13 @@
 #define ARVORE_AVL
 
 typedef struct registro reg;
-typedef struct arv* Arv;
+typedef struct no* Arv;
 
 reg* aloca_registro(char* id, int x, int y, int trafego);
 void printa_registro(reg *r);
 Arv cria_vazia();
+Arv cria_arv(reg* r, Arv sae, Arv sad);
+void exibe_arv(Arv A);
+void libera_arv(Arv* A);
 
 #endif
