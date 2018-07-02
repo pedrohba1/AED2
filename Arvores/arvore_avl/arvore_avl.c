6c47adbb3b4917e88f6515aded5f1c05b926e372
albanomatheus <albanomatheus16@gmail.com>
2018-07-02 16:05:00 -0300

    inserindo arvore avl, porem nao balanceando

--- Arvores/arvore_avl/arvore_avl.c
+++ Arvores/arvore_avl/arvore_avl.c
@@ -9,10 +9,10 @@ struct registro {
 };
 
 typedef struct no {
+	int h;
 	struct registro r;
 	struct no *sae;
 	struct no *sad;
-	int h;
 } no;
 
 reg* aloca_registro(char* id, int x, int y, int trafego) {
@@ -58,7 +58,7 @@ void printa_arv(Arv A) {
 	}	
 
 	printf("<");
-	printf("%s", A->r.id);
+	printf("%s", A->r.trafego);
 	printa_arv(A->sae);
 	printa_arv(A->sad);
 
@@ -81,3 +81,136 @@ void libera_arv(Arv* A) {
 	*A = NULL;
 }
 
+int alt_no(no* n) {
+	if (n == NULL) {
+		return -1;
+	}
+
+	return n->h;
+}
+
+int fatorBalancemento(Arv A) {
+	return alt_no(A->sae) - alt_no(A->sad);
+}
+
+int maior(int x, int y) {
+	if (x > y) {
+		return x;
+	} 	
+
+	return y;
+}
+
+int rot_dir(Arv *raiz) {
+	if (*raiz != NULL && (*raiz)->sae != NULL) {
+		Arv n = (*raiz)->sae;
+		(*raiz)->sae = n->sad;
+		n->sae = *raiz;
+		(*raiz)->h = maior(alt_no((*raiz)->sae),alt_no((*raiz)->sad))+1;
+		n->h = maior(alt_no(n->sae), (*raiz)->h)+1;
+		*raiz = n;
+
+		return 1;	
+	}
+
+	return 0;
+}
+
+int rot_esq(Arv *raiz) {
+	if (*raiz != NULL && (*raiz)->sad != NULL) {
+		Arv n = (*raiz)->sad;
+		(*raiz)->sad = n->sae;
+		n->sae = *raiz;
+		(*raiz)->h = maior(alt_no((*raiz)->sae),alt_no((*raiz)->sad))+1;
+		n->h = maior(alt_no(n->sae), (*raiz)->h)+1;
+		*raiz = n;
+
+		return 1;	
+	}
+	
+	return 0;
+}
+
+int rot_dir_esq(Arv *raiz) {
+	if (*raiz != NULL && (*raiz)->sae != NULL) {
+		rot_dir(&(*raiz)->sae);
+		rot_esq(raiz);
+		return 1;
+	}
+
+	return 0;
+}
+
+int rot_esq_dir(Arv *raiz) {
+	if (*raiz != NULL && (*raiz)->sad != NULL) {
+		rot_esq(&(*raiz)->sad);
+		rot_dir(raiz);
+		return 1;
+	}
+
+	return 0;
+}
+
+int balacearEsq(Arv* A) {
+	if (((*A)->sae)->h > 0) {
+		return rot_dir(A);
+	} else {
+		return rot_dir_esq(A);
+	}
+}
+
+int balacearDir(Arv* A) {
+	if (((*A)->sad)->h < 0) {
+		return rot_esq(A);
+	} else {
+		return rot_esq_dir(A);
+	}
+}
+
+
+int insere_avl(Arv* A, reg* elem, int* b) {
+	if (A == NULL) {
+		return 0;
+	}
+
+	if (*A == NULL) {
+		no* n = (no*)malloc(sizeof(no));
+		
+		n->r = *elem;
+		n->sae = NULL;
+		n->sad = NULL;
+		n->h = 0;
+
+		*A = n;
+		*b = 1;
+
+	} else if (elem->trafego <= (*A)->r.trafego) {
+		insere_avl(&(*A)->sae, elem, b);
+		if (*b == 1) {
+			if ((*A)->h == 1) {
+				balacearEsq(A);
+				*b = 0;
+			} else if ((*A)->h == 0) {
+				(*A)->h =1;
+			} else {
+				(*A)->h = 0;
+				*b = 0;
+			}
+		}
+	} else {
+		insere_avl(&(*A)->sad, elem, b);
+		if (*b == 1) {
+			if ((*A)->h == -1) {
+				balacearDir(A);
+				*b = 0;	
+			} else if ((*A)->h == 0) {
+				(*A)->h = -1;
+			} else {
+				(*A)->h = 0;
+				*b = 0;
+			}
+		}
+	}
+
+	return 1;
+}
--- Arvores/arvore_avl/main.c
+++ Arvores/arvore_avl/main.c
@@ -2,13 +2,25 @@
 #include "arvore_avl.h"
 
 int main() {
-	reg *r = aloca_registro("Manaus", 50, 300, 200);
-	printa_registro(r);
+	int b;
+	
+	reg *r1 = aloca_registro("Manaus", 50, 300, 200);
+	reg *r2 = aloca_registro("Rio De Janeiro", 50, 300, 250);
+	reg *r3 = aloca_registro("Sao Paulo", 50, 300, 320);
+	reg *r4 = aloca_registro("Uberlandia", 50, 300, 475);
+	reg *r5 = aloca_registro("Curitiba", 50, 300, 500);
+	reg *r6 = aloca_registro("Rio Grande do Sul", 50, 300, 600);
+	reg *r7 = aloca_registro("Mato Grosso", 50, 300, 550);
 
-	Arv A = cria_arv(r, NULL, NULL);
-	exibe_arv(A);
+	Arv A = cria_arv(r1, NULL, NULL);
 
-	libera_arv(&A);
+	insere_avl(&A, r2, &b);
+	insere_avl(&A, r3, &b);
+	insere_avl(&A, r4, &b);
+	insere_avl(&A, r5, &b);
+	insere_avl(&A, r6, &b);
+	insere_avl(&A, r7, &b);
+	
 	exibe_arv(A);
 	
 
