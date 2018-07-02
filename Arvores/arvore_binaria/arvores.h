c5b4a5d8fafdaf98d1f247e7f9208e0c3cc1fbff
pedrohba1 <pedro.almeida@ufu.br>
2018-07-02 00:49:40 -0300

    programa da árvore ABB finalizado. Criei o menu e a função de juntar que faltava

--- Arvores/arvore_binaria/arvores.h
+++ Arvores/arvore_binaria/arvores.h
@@ -4,7 +4,7 @@
 
 typedef struct no* Arv;
 typedef struct registro Registro;
-Registro* aloca_registro(int idade, char *nome);
+Registro* aloca_registro(int idade, char *nome, char *curso);
 Arv cria_vazia();
 Arv cria_arvore(Registro * rnew, Arv esq, Arv  dir);
 int arvore_vazia(Arv A);
@@ -17,11 +17,12 @@ int completa(Arv A);
 int altura_arv(Arv A);
 Arv juntar(Arv A1, Arv A2);
 
-//funções não pedidas
+//TAD
+int exibe_ordenado(Arv A);
 int insere_ord(Arv *A, Registro *r);
 int remove_ord(Arv* A, Registro * r);
 void printa_registro(Registro *r);
-
 int qtd_nos(Arv A);
+Arv busca_bin(Arv A, int idade);
 
 #endif // ARVORES_H_INCLUDED
